3e485fcdcbf46c8cc05b0b0240035b0fedda82f1
Richard Kim <rk.sungjin@gmail.com>
2018-07-18 15:44:56 -0700

    session

--- findcommonnumber.js
+++ findcommonnumber.js
@@ -14,3 +14,23 @@ for (i=0; i < arr.length; i++){
 return Math.max (Object.values(frequency));
 } // 2
 find_most_frequent_element(test);
+
+
+// //frequent element in an array
+// var test = [1, 2, 2, 3, 5];
+
+
+// function find_most_frequent_element(arr) {
+// var frequency = {};
+// var max = Math.max.apply( null, arr );
+// for (i=0; i < arr.length; i++){
+//   if (frequency[arr[i]])
+//   {
+//     frequency[arr[i]] = frequency[arr[i]] + 1;
+//   }  else {
+//     frequency[arr[i]] = 1;
+//   }
+// }
+// return max;
+// } // 2
+// find_most_frequent_element(test);
---
+++ practice-ES6.js
@@ -0,0 +1,13 @@
+let x = [1,2,3];
+let y = 4;
+
+// let z = x.concat(y)
+// console.log(x);
+
+let z = [...x ,y];
+console.log(z);
+
+// const y = () => (
+//     4;
+//   )
+//   console.log(y());
