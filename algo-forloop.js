65c71c48693658c15060753be86780356d7edf2a
Richard Kim <rk.sungjin@gmail.com>
2018-07-21 16:42:48 -0700

    question

--- algo-forloop.js
+++ algo-forloop.js
@@ -1,6 +1,16 @@
+//Print:
+//#
+//##
+//###
+//####
+//#####
+
 var str = "";
 
 for (var i = 1; i<6; i++) {
     str += "#"
     console.log(str);
 }
+
+// palidrone is str = revStr
+//"Otto" or "Madam" example
