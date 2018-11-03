696cae50df1f31a37f0f3b3c3c908b713d697ada
Richard Kim <rk.sungjin@gmail.com>
2018-11-02 18:00:10 -0700

    JS React Review

---
+++ JS_warmup_2.js
@@ -0,0 +1,43 @@
+class App extends Component {
+    render() {
+      return (
+        <Zillow unit='apartment1'>
+          {(data) => data === null
+            ? <Loading />
+            : <Badge info={data} />}
+        </Zillow>
+      )
+    }
+  }
+  
+  
+  import React, { Component, PropTypes } from 'react'
+  import fetchUnits from 'zillow'
+  // fetchUnits take in a unit’s name returns a promise
+  // which will resolve with that unit’s data.
+  
+  class Zillow extends Component {
+    //finish this...
+    constructor(props) {
+      super(props);
+  
+      this.state = { unit: null}; 
+    }
+    
+  
+    componentDidMount() {
+      fetchUnits(this.props.unit).then(function(unit) {
+        return this.setState( { unit } ) 
+      }).catch(....)
+    }
+    // fetchUnits(this.props) {
+      
+    // }
+  
+    render() {
+      return (
+  
+        this.props.children(this.state.unit)
+      )
+    }
+  }
